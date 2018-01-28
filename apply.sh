103d8365157acc6be7aa763c98ba30d946e6aacc
Jeffrey Sica <jeef111x@gmail.com>
2018-01-28 14:09:51 -0500

    no touchign

--- apply.sh
+++ apply.sh
@@ -4,18 +4,20 @@
 
 find . -type f -name \*.yaml -print0 | while read -d $'\0' file
 do
-  if [[ $file =~ .*configmap.* ]]; then
-    echo "-- Configmap found";
-    if ! kubectl get -f $file > /dev/null 2>&1; then
-      #kubectl apply -f "$file"
-      echo "kubectl apply -f \"$file\""
+  if [[ ! $file =~ .*NO_TOUCHING.* ]]; then
+    if [[ $file =~ .*configmap.* ]]; then
+      echo "-- Configmap found";
+      if ! kubectl get -f $file > /dev/null 2>&1; then
+        kubectl apply -f "$file"
+        echo "kubectl apply -f \"$file\""
+      else
+        kubectl replace --force -f "$file"
+        echo "kubectl replace -f \"$file\""
+      fi
     else
-      #kubectl replace -f "$file"
-      echo "kubectl replace -f \"$file\""
+      echo "kubectl apply -f $file"
+      kubectl apply -f "$file"
     fi
-  else
-    echo "kubectl apply -f $file"
-    #kubectl apply -f "$file"
   fi
 done
 
